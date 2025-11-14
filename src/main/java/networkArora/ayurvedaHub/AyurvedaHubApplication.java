bafbb27fc3e1b475c62caece5995e613379a2e35
Aman Arora <amanarora99005@gmail.com>
2025-11-14 23:38:54 +0530

    Inital Commit

---
+++ src/main/java/networkArora/ayurvedaHub/AyurvedaHubApplication.java
@@ -0,0 +1,13 @@
+package networkArora.ayurvedaHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyurvedaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyurvedaHubApplication.class, args);
+	}
+
+}
